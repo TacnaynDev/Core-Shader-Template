e0c9430920cc35f7b0e251d791d86b2fa70403c1
Ian Knecht <92757599+IanK9988@users.noreply.github.com>
2022-10-16 23:59:52 -0400

    Update config.glsl with an example

--- assets/minecraft/shaders/include/config.glsl
+++ assets/minecraft/shaders/include/config.glsl
@@ -1,5 +1,2 @@
-// How many blocks above the camera to render
-#define UPPER_BLOCK_CLIPPING_PLANE 7.0
-
-// How many blocks below the camera to render
-#define LOWER_BLOCK_CLIPPING_PLANE 20.0
+// Add configuration variables here
+#define EXAMPLE_VARIABLE 1.0
